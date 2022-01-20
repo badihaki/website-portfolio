6ad6bf219f1831b3185022262be81eeaf646725f
Badi-Haki Martin <jojackblack@gmail.com>
2022-01-20 12:38:22 -0500

    Completed my navigation loop using script.js. started writing about.html and gallery.html

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 /*
 starting stuff
 */
-/* this below works
-const h2=document.createElement("h2");
-h2.textContent="This content was made with JavaScript";
+lineBreak = document.createElement("br");
 
-document.querySelector("body").appendChild(h2);
-just gotta work on other stuff rn
-*/
+const navi=document.createElement("div");
+navi.id="navi";
+document.querySelector("body").appendChild(navi);
+
+const naviHeader=document.createElement("h2");
+naviHeader.textContent="Navigation";
+navi.appendChild(naviHeader);
+
+const linkHome=document.createElement("a");
+linkHome.href="./index.html";
+linkHome.textContent="Home";
+navi.appendChild(linkHome);
+
+navi.appendChild(document.createElement("br"));
+
+const linkGallery=document.createElement("a");
+linkGallery.href="./gallery.html";
+linkGallery.textContent="Gallery";
+navi.appendChild(linkGallery);
+
+navi.appendChild(document.createElement("br"));
+
+const linkAbout=document.createElement("a");
+linkAbout.href="./about.html";
+linkAbout.textContent="About";
+navi.appendChild(linkAbout);
+
+navi.appendChild(document.createElement("br"));
 
 
 
