a0d3e17d8a89ad1550ec1455dc22feab19d9a691
Badi-Haki Martin <jojackblack@gmail.com>
2022-01-20 01:24:08 -0500

    modified index.html and style.css. Trying to hone into a unified style. Added images folder. Added 'minime' image, and put it in index.html

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+/*
+starting stuff
+*/
+/* this below works
 const h2=document.createElement("h2");
 h2.textContent="This content was made with JavaScript";
 
 document.querySelector("body").appendChild(h2);
+just gotta work on other stuff rn
+*/
+
+
+
+
+/*navigation*/
+/*
+const navi=document.createElement("div");
+navi.style.cssText='left:0px;';
+*/
+/*adding navigation links*/
+/*
+const indexLink=navi.createElement("a");
+indexLink.setAttribute("href", "./index.html");
+indexLink.textContent="Home";
+/*
+/*adding the navi element*/
+/*
+document.querySelector("body").appendChild(navi);
+*/
